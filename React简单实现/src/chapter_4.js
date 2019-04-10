ca713d3b5a89e035701e8defa117ae4af8c8287f
LemonsHi <wulemons@gmail.com>
2019-04-10 17:47:02 +0800

    fix

---
+++ React简单实现/src/chapter_4.js
@@ -0,0 +1,38 @@
+/**************************************************************************************/
+/* 参考文章地址：https://github.com/hujiulong/simple-react/blob/chapter-4/src/index.js */
+/**************************************************************************************/
+
+import React from './react'
+import ReactDOM from './react-dom'
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            num: 0
+        }
+    }
+    componentDidMount() {
+        for ( let i = 0; i < 100; i++ ) {
+            this.setState( prevState => {
+                console.log( prevState.num );
+                return {
+                    num: prevState.num + 1
+                }
+            } );
+        }
+    }
+    componentDidUpdate() {
+        console.log( 'update' )
+    }
+    render() {
+        return ( <div className='App'>
+            <h1>{this.state.num}</h1>
+        </div> );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById( 'root' )
+);
